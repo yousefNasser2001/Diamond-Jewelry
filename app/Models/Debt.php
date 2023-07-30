fd396b30535a9877c279e84acddab8f22d3fe101
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-30 16:33:30 +0300

    refactor model

--- app/Models/Debt.php
+++ app/Models/Debt.php
@@ -4,15 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Debt extends Model
 {
-    use HasFactory , SoftDeletes;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['person_name', 'amount', 'debt_date', 'is_debt_from_others' ,'currency_id' ,'is_paid'];
+    protected $fillable = [
+        'person_name',
+        'amount',
+        'debt_date',
+        'is_debt_from_others',
+        'currency_id',
+        'is_paid',
+    ];
 
-    public function currency()
+    public function currency(): BelongsTo
     {
         return $this->belongsTo(Currency::class);
     }
