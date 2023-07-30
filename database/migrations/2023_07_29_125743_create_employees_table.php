3fccfb7d717f5b74748d36351960293f8c13cf1b
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-30 15:19:17 +0300

    refactor table

--- database/migrations/2023_07_29_125743_create_employees_table.php
+++ database/migrations/2023_07_29_125743_create_employees_table.php
@@ -4,14 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+return new class extends Migration {
+    public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
@@ -24,11 +18,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('employees');
