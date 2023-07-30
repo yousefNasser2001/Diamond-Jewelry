e38c06b14c18cf72e4dc8fa4b166e8abdb8b32fa
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-07-30 12:40:11 +0300

    add js Files

---
+++ public/assets/js/custom/apps/debts/view/update-debt-en.js
@@ -0,0 +1,200 @@
+"use strict";
+
+$("#kt_modal_add_debts_datepicker").flatpickr({
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+});
+
+// Class definition
+const KTdebtsUpdateDetails = function () {
+    // Shared variables
+    const element = document.getElementById('kt_modal_update_details');
+    const form = element.querySelector('#kt_modal_update_debt_form');
+    const modal = new bootstrap.Modal(element);
+
+    // Init add schedule modal
+    const initUpdateDetails = () => {
+
+        // Close button handler
+        const closeButton = element.querySelector('[data-kt-debts-modal-action="close"]');
+        closeButton.addEventListener('click', e => {
+            e.preventDefault();
+
+            Swal.fire({
+                text: "Are you sure you would like to cancel?",
+                icon: "warning",
+                showCancelButton: true,
+                buttonsStyling: true,
+                confirmButtonText: "Yes, cancel it!",
+                cancelButtonText: "No, return",
+                customClass: {
+                    confirmButton: "btn btn-primary",
+                    cancelButton: "btn btn-active-light"
+                }
+            }).then(function (result) {
+                if (result.value) {
+                    form.reset(); // Reset form
+                    modal.hide();
+                } else if (result.dismiss === 'cancel') {
+                    Swal.fire({
+                        text: "Your form has not been cancelled!.",
+                        icon: "error",
+                        buttonsStyling: false,
+                        confirmButtonText: "Ok, got it!",
+                        customClass: {
+                            confirmButton: "btn btn-primary",
+                        }
+                    });
+                }
+            });
+        });
+
+        // Cancel button handler
+        const cancelButton = element.querySelector('[data-kt-debts-modal-action="cancel"]');
+        cancelButton.addEventListener('click', e => {
+            e.preventDefault();
+
+            Swal.fire({
+                text: "Are you sure you would like to cancel?",
+                icon: "warning",
+                showCancelButton: true,
+                buttonsStyling: true,
+                confirmButtonText: "Yes, cancel it!",
+                cancelButtonText: "No, return",
+                customClass: {
+                    confirmButton: "btn btn-primary",
+                    cancelButton: "btn btn-active-light"
+                }
+            }).then(function (result) {
+                if (result.value) {
+                    form.reset(); // Reset form
+                    modal.hide();
+                } else if (result.dismiss === 'cancel') {
+                    Swal.fire({
+                        text: "Your form has not been cancelled!.",
+                        icon: "error",
+                        buttonsStyling: false,
+                        confirmButtonText: "Ok, got it!",
+                        customClass: {
+                            confirmButton: "btn btn-primary",
+                        }
+                    });
+                }
+            });
+        });
+
+        const validator = FormValidation.formValidation(
+            form,
+            {
+                fields: {
+                    'person_name': {
+                        validators: {
+                            notEmpty: {
+                                message: 'person name is required',
+                            }
+                        }
+                    },
+                    'amount': {
+                        validators: {
+                            notEmpty: {
+                                message: 'amount is required',
+                            }
+                        }
+                    },
+                    'currency_id': {
+                        validators: {
+                            notEmpty: {
+                                message: 'the currency is required',
+                            }
+                        }
+                    },
+                    'debt_date': {
+                        validators: {
+                            notEmpty: {
+                                message: 'debt date is required',
+                            }
+                        }
+                    },
+                },
+
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger(),
+                    bootstrap: new FormValidation.plugins.Bootstrap5({
+                        rowSelector: '.fv-row',
+                        eleInvalidClass: '',
+                        eleValidClass: ''
+                    })
+                }
+            }
+        );
+
+        // Submit button handler
+        const submitButton = element.querySelector('[data-kt-debts-modal-action="submit"]');
+        submitButton.addEventListener('click', function (e) {
+            // Prevent default button action
+            e.preventDefault();
+
+            if (validator) {
+                validator.validate().then(function (status) {
+                    if (status === 'Valid') {
+                        // Show loading indication
+                        submitButton.setAttribute('data-kt-indicator', 'on');
+
+                        // Disable button to avoid multiple click
+                        submitButton.disabled = true;
+
+                        // Simulate form submission. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                        setTimeout(function () {
+                            // Remove loading indication
+                            submitButton.removeAttribute('data-kt-indicator');
+
+                            // Enable button
+                            submitButton.disabled = false;
+
+                            // Show popup confirmation
+                            Swal.fire({
+                                text: "Form has been successfully submitted!",
+                                icon: "success",
+                                buttonsStyling: false,
+                                confirmButtonText: "Ok, got it!",
+                                customClass: {
+                                    confirmButton: "btn btn-primary"
+                                }
+                            }).then(function (result) {
+                                if (result.isConfirmed) {
+                                    modal.hide();
+                                }
+                            });
+
+                            form.submit(); // Submit form
+                        }, 2000);
+                    } else {
+                        // Show popup warning. For more info check the plugin's official documentation: https://sweetalert2.github.io/
+                        Swal.fire({
+                            text: "Sorry, looks like there are some errors detected, please try again.",
+                            icon: "error",
+                            buttonsStyling: false,
+                            confirmButtonText: "Ok, got it!",
+                            customClass: {
+                                confirmButton: "btn btn-primary"
+                            }
+                        });
+                    }
+                })
+            }
+
+        });
+    };
+
+    return {
+        // Public functions
+        init: function () {
+            initUpdateDetails();
+        }
+    };
+}();
+
+// On document ready
+KTUtil.onDOMContentLoaded(function () {
+    KTdebtsUpdateDetails.init();
+});
