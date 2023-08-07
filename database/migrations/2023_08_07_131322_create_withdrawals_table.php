343942bfac70ac357a998bb649d669e783cb634d
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-08-07 14:44:45 +0300

    withdrawals table

---
+++ database/migrations/2023_08_07_131322_create_withdrawals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('withdrawals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id');
+            $table->decimal('amount' ,AMOUNT_TOTAL_DIGITS, AMOUNT_TOTAL_DIGITS_FLOAT);
+            $table->dateTime('date');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('withdrawals');
+    }
+};
