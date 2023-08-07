bac89bd38c5b52a2b5d35824288cd3293ba27ff6
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-08-07 14:44:34 +0300

    withdrawal seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             EmployeeSeeder::class,
             DebtSeeder::class,
             ExpenseSeeder::class,
+            // WithdrawalSeeder::class,
         ]);
     }
 }
