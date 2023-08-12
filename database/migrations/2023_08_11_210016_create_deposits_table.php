b59f5d6ceea8bf0110fb13360d7e64779bd942c6
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-08-12 16:52:44 +0300

    Deposit CRUD model -mcsf

---
+++ database/migrations/2023_08_11_210016_create_deposits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deposits', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', AMOUNT_TOTAL_DIGITS, AMOUNT_TOTAL_DIGITS_FLOAT);
+            $table->foreignId('currency_id');
+            $table->text('notes')->nullable();
+            $table->dateTime('deposit_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deposits');
+    }
+};
