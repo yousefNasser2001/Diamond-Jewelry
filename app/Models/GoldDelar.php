4b1a044be1373ca98068c046a38ab68378ccb711
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-08-13 13:50:28 +0300

    GoldDelar model

---
+++ app/Models/GoldDelar.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class GoldDelar extends Model
+{
+    use HasFactory , SoftDeletes;
+
+    protected $fillable = ['name' , 'total_weight' , 'total_workmanship' ,'phone_number'];
+
+    public function goldTransactions(): HasMany
+    {
+        return $this->hasMany(GoldTransaction::class, 'gold_delar_id');
+    }
+}
