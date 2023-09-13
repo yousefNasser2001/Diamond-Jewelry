506bf881c0de574c6bda53b9692e602b631b7400
yousefNasser2001 <yousef.naseer26@gmail.com>
2023-09-13 18:25:55 +0300

    refactor Debt model

--- app/Models/Debt.php
+++ app/Models/Debt.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -14,22 +13,76 @@ class Debt extends Model
 
     protected $fillable = [
         'person_name',
-        'amount',
+        'shekels_balance',
+        'dollars_balance',
+        'dinars_balance',
         'debt_date',
         'is_debt_from_others',
-        'currency_id',
         'is_paid',
         'weight',
         'phone_number',
     ];
 
-    public function currency(): BelongsTo
-    {
-        return $this->belongsTo(Currency::class);
-    }
-
     public function debt_transactions(): HasMany
     {
-        return $this->hasMany(DebtTransaction::class , 'person_id');
+        return $this->hasMany(DebtTransaction::class, 'person_id');
+    }
+
+    public function shekels_balance()
+    {
+        $income = $this->debt_transactions()
+            ->where('currency_id', 2)
+            ->where('transaction_type', 'سحب')
+            ->sum('amount');
+
+        $expense = $this->debt_transactions()
+            ->where('currency_id', 2)
+            ->where('transaction_type', 'سداد')
+            ->sum('amount');
+
+        return $this->shekels_balance + $income - $expense;
+    }
+
+    public function dollars_balance()
+    {
+        $income = $this->debt_transactions()
+            ->where('currency_id', 1)
+            ->where('transaction_type', 'سحب')
+            ->sum('amount');
+
+        $expense = $this->debt_transactions()
+            ->where('currency_id', 1)
+            ->where('transaction_type', 'سداد')
+            ->sum('amount');
+
+        return $this->dollars_balance + $income - $expense;
+    }
+
+    public function dinars_balance()
+    {
+        $income = $this->debt_transactions()
+            ->where('currency_id', 3)
+            ->where('transaction_type', 'سحب')
+            ->sum('amount');
+
+        $expense = $this->debt_transactions()
+            ->where('currency_id', 3)
+            ->where('transaction_type', 'سداد')
+            ->sum('amount');
+
+        return $this->dinars_balance + $income - $expense;
+    }
+
+    public function weight()
+    {
+        $income = $this->debt_transactions()
+            ->where('transaction_type', 'سحب')
+            ->sum('weight');
+
+        $expense = $this->debt_transactions()
+            ->where('transaction_type', 'سداد')
+            ->sum('weight');
+
+        return $this->weight + $income - $expense;
     }
 }
